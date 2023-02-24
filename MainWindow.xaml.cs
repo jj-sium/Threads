980e335f6c180aa92f7b02bacdceb71ce6f51ef5
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-02-24 09:22:33 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ghinelli.johan._4h.threads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int _counter = 0;
+        const int NGIRI = 100;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(Incrementare);
+            t.Start();
+        }
+
+        void Incrementare()
+        {
+            for(int x = 0; x < NGIRI; x++)
+            {
+                _counter++;
+                Dispatcher.Invoke(
+                    () =>
+                    {
+                       counter1.Text = _counter.ToString();
+                    }
+                );
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
